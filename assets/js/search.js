80a4e7e687b2ce58997a5f2da4637ee66bfa7efe
KurongTohsaka <KurongTohsaka@users.noreply.github.com>
2026-06-12 11:56:41 +0000

    deploy: 4dd379bca87e35d4222fa1868ec70394f5793064

--- assets/js/search.js
+++ assets/js/search.js
@@ -241,7 +241,7 @@
     if (h.length === 0) { searchResults.innerHTML = ""; return; }
     var html = '<div class="search-history"><div class="search-history-title">最近搜索</div>';
     for (var i = 0; i < h.length; i++) {
-      html += '<span class="search-history-item"><span class="search-history-text">' + escapeHtml(h[i]) + '</span><button class="search-history-del" data-query="' + escapeHtml(h[i]) + '" title="移除">&times;</button></span>';
+      html += '<span class="search-history-item"><span class="search-history-text">' + escapeHtml(h[i]) + '</span><button class="search-history-del" data-query="' + escapeHtml(h[i]) + '" title="移除"><svg viewBox="0 0 16 16"><line x1="4" y1="4" x2="12" y2="12"/><line x1="12" y1="4" x2="4" y2="12"/></svg></button></span>';
     }
     html += '</div>';
     searchResults.innerHTML = html;
